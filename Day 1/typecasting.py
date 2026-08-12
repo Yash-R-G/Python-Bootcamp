5579d92c9890c209d18c44cae5d4751c81613127
Yash Raikar <yr.ctrlsec@gmail.com>
2026-08-12 12:24:46 +0530

    Create typecasting.py

---
+++ Day 1/typecasting.py
@@ -0,0 +1,32 @@
+#test all you know
+
+name = "Yash"
+
+employee_id = "PY1024"
+
+department = "Python Development"
+
+experience = 0
+
+salary = 80000
+
+
+print("========== Employee Badge ==========")
+
+print(f"Name         : {name}")
+
+print(f"Employee ID  : {employee_id}")
+
+print(f"Department   : {department}")
+
+print(f"Experience   : {experience} years")
+
+print(f"Salary       : ${salary:,}")
+
+print("====================================")
+
+
+print(bool(""))
+print(bool("Python"))
+print(bool(0))
+print(bool(1))

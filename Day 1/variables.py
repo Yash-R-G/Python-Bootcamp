47be44a6e60aa1dbab7e9e466a91b59b8d5daefb
Yash Raikar <yr.ctrlsec@gmail.com>
2026-08-12 12:23:40 +0530

    Create variables.py

---
+++ Day 1/variables.py
@@ -0,0 +1,28 @@
+# this is my first time writing python progarming
+
+# Strings
+
+Name = "bro"
+food = "pizza"
+email = "xyx@fake.com"
+
+# print(f"Your name is {Name}\nYour favorite food is {food}\nThis is your current email {email}, please contact help desk for changing email")
+
+# Integers 
+
+EmployeeNo = 35493728
+age = 20
+no_of_employee = 56
+
+
+# Float 
+
+gpa = 5.45
+price = 23.33
+distance = 5.6
+
+# Bolean
+
+Unemployement = True
+for_sale = False
+is_online = 1

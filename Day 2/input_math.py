78f83466e30c947a38c6b205b66a69e355c43868
Yash Raikar <yr.ctrlsec@gmail.com>
2026-08-12 12:43:32 +0530

    Create input_math.py

---
+++ Day 2/input_math.py
@@ -0,0 +1,7 @@
+#  simple math ( only plus )
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+output = num1 + num2
+
+print(f"The total is: {output}")

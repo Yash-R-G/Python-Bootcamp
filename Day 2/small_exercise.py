68f46016ce85d0aa4f3cacabc9e59bf045454f02
Yash Raikar <yr.ctrlsec@gmail.com>
2026-08-12 12:47:29 +0530

    Create small_exercise.py

---
+++ Day 2/small_exercise.py
@@ -0,0 +1,48 @@
+# Exercise 1 calulate the area of a rectangle 
+
+# length = float(input("Enter The Length : "))
+# width = float(input("Enter The Width : "))
+
+# area = length * width
+
+# print(f"The area of a rectangle is {area}cm²")
+
+# Exercise 2 Shopping cart program 
+
+# item = input("what would you like to buy : ")
+# price = float(input(f"what the price of the listed item ({item}) : "))
+# quantity = int(input("how much would you like to buy : "))
+
+# total = price * quantity
+
+# print(f"You have bought {quantity} x {item}/s ")
+# print(f"Your total is : ₹{total}") 
+
+
+# Exercise 3
+
+
+# employee_name = input("Enter your Employee Name : " )
+# hours_worked = float(input("Enter the hours that have you worked : "))
+# hourly_wage = float(input("Enter your hourly wage : "))
+# bonus = float(input("Enter the bonus, if you dont get any bonus enter 0 : "))
+
+# salary = hours_worked * hourly_wage
+# final_salary = salary + bonus
+# tax = salary * 0.10
+# net_salary = final_salary - tax
+
+# title = "Salary Slip"
+
+# print(f"======================{title}======================\n")
+
+# print(f"Employee Name : {employee_name}")
+# print(f"Hours Worked  : {hours_worked} ")
+# print(f"Hourly Wage   : ${hourly_wage}")
+# print(f"Gross Salary  : ${salary:,.2f}")
+# print(f"Bonus         : ${bonus:,.2f}")
+# print(f"Final Salary  : ${final_salary:,.2f}")
+# print(f"Tax (10%)     : ${tax:,.2f}")
+# print(f"Net Salary    : ${net_salary:,.2f}\n")
+
+# print("=======================================================")   
